c6c00404c49d0d8c33da81ca94a22d10176d4b20
L16H7 <linmyatko@hotmail.com>
2020-05-15 21:55:25 +0630

    list ui

--- client/src/views/ContentListPage.vue
+++ client/src/views/ContentListPage.vue
@@ -2,6 +2,14 @@
   <v-container>
     Content List Page
     {{ video }}
+    <iframe
+      style="width: 100%;"
+      v-if="video.content"
+      :src="video.content"
+      frameborder="0"
+      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+    />
   </v-container>
 </template>
 
