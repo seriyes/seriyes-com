d35d836dcdfbf47210cd0fbe65d59338baa38919
L16H7 <linmyatko@hotmail.com>
2020-05-20 21:02:59 +0630

    eslint fixed

--- server/src/modules/Notes/resolvers.js
+++ server/src/modules/Notes/resolvers.js
@@ -11,12 +11,6 @@ const resolvers = {
       const ret = await Note.find({
         userId: '001',
       });
-      console.log({ ret });
-      let [first] = ret;
-      // first = first.toJSON();
-      console.log({ first });
-      console.log({ title: first.title });
-      console.log({ userId: first.userId });
 
       return ret;
     },
